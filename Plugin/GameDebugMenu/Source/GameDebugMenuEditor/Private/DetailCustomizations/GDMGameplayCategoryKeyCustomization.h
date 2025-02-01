4feb5cc48bfdedf8fc608c32143e0289630fe8e0
000-aki-000 <0224akihiko@gmail.com>
2025-02-01 11:21:00 +0900

    [Update]プラグインフォルダ更新

--- Plugin/GameDebugMenu/Source/GameDebugMenuEditor/Private/DetailCustomizations/GDMGameplayCategoryKeyCustomization.h
+++ Plugin/GameDebugMenu/Source/GameDebugMenuEditor/Private/DetailCustomizations/GDMGameplayCategoryKeyCustomization.h
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2024 akihiko moroi
+* Copyright (c) 2025 akihiko moroi
 *
 * This software is released under the MIT License.
 * (See accompanying file LICENSE.txt or copy at http://opensource.org/licenses/MIT)
